7be47ccb2d95bb1186d6cf49439d4f7a53303597
dagbay <80764810+dagbay@users.noreply.github.com>
2023-02-03 15:22:56 +1100

    Added property class

---
+++ lib/property.rb
@@ -0,0 +1,20 @@
+class Property
+    def initialize(name, price, colour, type)
+        @name = name
+        @price = price
+        @colour = colour
+        @type = type
+    end
+    def name 
+        @name
+    end
+    def price
+        @price
+    end
+    def colour
+        @colour
+    end
+    def type
+        @type
+    end
+end
