4c80b8bce207116cf8f907950b5e688dbf5d5945
dagbay <80764810+dagbay@users.noreply.github.com>
2023-02-04 17:03:29 +1100

    Changed test cases due to only using one json file for the board

--- spec/features/board_spec.rb
+++ spec/features/board_spec.rb
@@ -1,19 +1,13 @@
 require 'board'
+require 'json'
 RSpec.describe Board do
     before do
         @board = Board.new
-        @json = { :name=>'DummyName', :type=>'DummyType' }.to_json
+        @json = 'board_test.json'
     end
     describe 'JSON file' do
-        context 'should be loaded do' do
-            it 'given all information is there' do
-                # expect(@board.load_board()).to
-            end
-        end
-        context 'should still be loaded' do
-            it 'given there is no price and colour' do
-                expect(@board.load_board(@json)).to eq(@json)
-            end
+        it 'should be loaded' do
+            expect(@board.load_board(@json).class).to eq(Array)
         end
     end
 
