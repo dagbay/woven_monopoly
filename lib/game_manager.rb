13928328d447924eede79a5318830527e44ff91d
dagbay <80764810+dagbay@users.noreply.github.com>
2023-02-05 15:57:15 +1100

    Added load functionalities

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -1,3 +1,21 @@
+require 'json'
+require_relative 'board'
+require_relative 'player'
 class GameManager
-    
+    def load_board
+        @board = Board.new.load_properties('board.json')
+        @board
+    end
+    def load_players
+        @players = Array.new
+        file = File.read('players.json')
+        file_data = JSON.parse(file)
+        file_data.each do |name|
+            @players << Player.new(name)
+        end
+        @players
+    end
 end
+
+game = GameManager.new
+game.load_players
