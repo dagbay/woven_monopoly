5317e73cedb81cbf9c894d109859e6bf1dc08862
dagbay <80764810+dagbay@users.noreply.github.com>
2023-02-05 16:09:57 +1100

    Added indicators

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -4,6 +4,7 @@ require_relative 'player'
 class GameManager
     def load_board
         @board = Board.new.load_properties('board.json')
+        puts "The board has been loaded!"
         @board
     end
     def load_players
@@ -12,10 +13,10 @@ class GameManager
         file_data = JSON.parse(file)
         file_data.each do |name|
             @players << Player.new(name)
+            puts "New Player: #{name} is now playing!"
         end
         @players
     end
 end
 
 game = GameManager.new
-game.load_players
