7255dc63170d4f16bd218cda9792abb46e5f8255
dagbay <80764810+dagbay@users.noreply.github.com>
2023-02-05 21:50:27 +1100

    Added check if player looped board

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,10 @@
+require 'colorize'
 class Player 
     def initialize(name)
         @name = name
         @balance = 16
-        @position = 0
+        @position = 0 
+        @looped = false
     end
     # Get Functions
     def get_balance
@@ -14,23 +16,38 @@ class Player
     def get_position
         @position
     end
+    def check_loop
+        @looped
+    end
     # Set Functions
     def pay_rent(num)
         @balance -= num
         if @balance > 0
-            puts "You paid $#{num}. You now have $#{@balance} left in your balance."
+            puts "You paid $#{num}. You now have $#{get_balance} left in your balance."
         else
             puts "You are bankrupt!"
         end
     end
     def move_by(num, limit)
+        puts "\n--- #{get_name}'s Turn ---".green
         @position += num
-        puts "#{@name} moved #{num} times up the board"
-        if @position >= limit
-            @position = @position - limit
-            puts "However, #{@name} has reached the limit and has looped around the board. And is now at position: #{@position}"
+        puts "· #{get_name} moved #{num} times up the board.".red
+        if get_position >= limit
+            @position = get_position - limit
+            puts "· However, #{get_name} has looped around the board and is now at position: #{get_position}.".red
         else
-            puts "#{@name} is now at position: #{@position}"
+            puts "· #{get_name} is now at position: #{get_position}.".red
         end
+        puts "--- End of Turn ---\n".green
+    end
+    def add_loop
+        @looped = true
     end
 end
+
+# TODO: Complete the move_by logic
+# TODO: Check if you can make a generic function for colorizing the console
+
+player = Player.new("Daniel")
+player.move_by(3,10)
+player.move_by(7,10)
