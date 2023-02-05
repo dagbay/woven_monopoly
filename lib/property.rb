430d7657fe772ef8f34d9b9f518f741e7e6cc2aa
dagbay <80764810+dagbay@users.noreply.github.com>
2023-02-05 18:59:43 +1100

    Refactored property.rb to have encapsulation

--- lib/property.rb
+++ lib/property.rb
@@ -1,10 +1,21 @@
 class Property
-    attr_accessor :name, :price, :colour, :type
     def initialize(hash)
         @name = hash['name']
         @price = hash['price'].nil? ? "n/a" : hash['price']
         @colour = hash['colour'].nil? ? "n/a" : hash['colour']
         @type = hash['type']
+    end
+    def get_name
+        @name
+    end
+    def get_price
+        @price
+    end
+    def get_colour
+        @colour
+    end
+    def get_type
+        @type
     end
     def get_hash
         @hash = { :name => @name, :price => @price, :colour => @colour, :type => @type }
