56e097bb13112d360860336f47e0a20b8cdbe8af
dagbay <80764810+dagbay@users.noreply.github.com>
2023-02-07 17:11:29 +1100

    Optimize code and added user prompts

--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,6 @@
 require_relative 'game_manager'
+require 'colorize'
+require 'io/console'
 
 # Create game
 game = GameManager.new
@@ -6,19 +8,30 @@ game.load_board
 game.load_players
 game.determine_game_from_roll('rolls_1.json')
 
-# Initialize index
+# Initialize variables
+dice_rolls = game.get_dice_rolls
+players = game.get_players
+board = game.get_board
 current_roll = 0
 current_player = 0
+continue = false
 
 # Game Loop
-while current_roll < game.get_dice_rolls.length do 
+while current_roll < dice_rolls.length do 
 	# Get Current Roll
-	roll = game.get_dice_rolls[current_roll]
+	roll = dice_rolls[current_roll]
 
 	# Game Logic
-	game.get_players[current_player].move_by(roll, game.get_board.length - 1)
+	players[current_player].move_by(roll, board.length - 1)
+
+    # Prompt for next turn or end of loop
+    unless continue == true
+        print "Press any key to continue or key \'Esc\' to go to the end of the loop: ".blue
+        input_value = $stdin.getch
+        continue = true if input_value == "\e"
+    end
 
 	# Go to next player and next roll
-	current_player = (current_player + 1) % game.get_players.length
+	current_player = (current_player + 1) % players.length
 	current_roll += 1
 end
